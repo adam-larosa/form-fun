92e3fc097713c597b380ba22b71701439518e46b
Adam La Rosa <adam.larosa@flatironschool.com>
2023-06-15 12:59:04 -0700

    fin

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import { useState } from 'react'
 import TheForm from './TheForm'
 import TheDisplay from './TheDisplay'
 
+const anArrayOfNameObjects = [ 
+	{ name: 'baxter' } 
+]
+
 function App() {
-	const [ names ] = useState( [ { name: 'baxter' } ] ) 
+	
+	const [ namesArray, setNames ] = useState( anArrayOfNameObjects )
+
+	const addNameToArray = newObj => {
+		// when changing state, you always change to "a copy of"
+		const newArray = [ ...namesArray ]
+		// modify the new copy
+		newArray.push( newObj )
+		//change state to the new copy
+		setNames( newArray )
+
+		// same as above but one-liner
+		// setNames( [ ...namesArray, newObj ] )
+	}
+
+	const removeFromArray = doomedID => {
+		setNames( namesArray.filter( nObj => nObj.id !== doomedID ))
+	}
+
+	const updateExistingObj = freshObj => {
+		// tbc...  ;) 
+	}
+	
 	return (
 		<div>
-			<TheForm />
-			<TheDisplay names={ names } />
+			<TheForm addNameToArray={ addNameToArray } />
+			<TheDisplay names={ namesArray } />
 		</div>
 	);
 }
--- src/TheForm.js
+++ src/TheForm.js
@@ -1,7 +1,36 @@
-export default function TheForm() {
+
+import { useState } from 'react'
+
+export default function TheForm({ addNameToArray }) {
+
+
+    const [ name, setName ] = useState( '' )
+
+
+    const handleName = e => {
+        setName( e.target.value )
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const newObj = { name: name }
+        // the JavaScript object shorthand: const newObj = { name: name }
+
+        addNameToArray( newObj )
+        e.target.reset()
+        setName( '' )
+    }
+    
     return (
-        <form>
-            <input name="name" />
+        <form onSubmit={ handleSubmit }>
+            {/* every input that the user gives info to, 
+                should have its own state */}
+            <input onChange={ handleName } name="name" />
+            {/* the input also needs, an onChange listener, that will
+                allow the state to be changed */}
+
+
+
             <input type="submit" />
         </form>
     )
